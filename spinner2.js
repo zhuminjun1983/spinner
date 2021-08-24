2517364a4139b07cc141f137fadb61e178d84a1f
zhuminjun1983 <87101201+zhuminjun1983@users.noreply.github.com>
2021-08-24 13:04:25 -0700

    new spin

---
+++ spinner2.js
@@ -0,0 +1,13 @@
+setTimeout(() => {
+  process.stdout.write('\r|   ');
+}, 100);
+
+setTimeout(() => {
+  process.stdout.write('\r/   ');
+}, 300);
+
+setTimeout(() => {
+  process.stdout.write('\r-   ');
+}, 500);
+
+
